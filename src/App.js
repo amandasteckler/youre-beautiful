a1cdfe4e3f479a19508a75ba3c15bddb9d62194f
Amanda Steckler <amandajsteckler@gmail.com>
2016-12-27 13:40:28 +0000

    Setup form for name

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+
+      <h1>What is your name?</h1>
+      <form>
+        <input type="text" />
+        <button>Submit</button>
+      </form>
+      </div>
+
+    );
+  }
+}
+
+export default App;
